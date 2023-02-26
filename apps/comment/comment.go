c2dbe13b5224f1c8fd0b69c18af74917a985a944
Baytan <baytan2@hotmail.com>
2023-02-27 03:43:08 +0800

    现在comment启动会将评论id写入缓存

--- apps/comment/comment.go
+++ apps/comment/comment.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"OutTiktok/dao"
 	"flag"
 	"fmt"
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/zero-contrib/zrpc/registry/consul"
+	"gorm.io/gorm"
+	"strconv"
 	"time"
 
 	"OutTiktok/apps/comment/comment"
@@ -26,6 +30,7 @@ func main() {
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 	ctx := svc.NewServiceContext(c)
+	initComment(ctx.Redis, ctx.DB)
 
 	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
 		comment.RegisterCommentServer(grpcServer, server.NewCommentServer(ctx))
@@ -41,4 +46,26 @@ func main() {
 
 	fmt.Printf("[%s] Starting rpc server at %s...\n", time.Now().Format("2006-01-02 15:04:05"), c.ListenOn)
 	s.Start()
+}
+
+// 将评论ID写入缓存
+func initComment(Redis *redis.Redis, DB *gorm.DB) {
+	videoIds, err := Redis.Zrange("feed", 0, -1)
+	if err != nil {
+		panic(err)
+	}
+	for _, id := range videoIds {
+		var comments []*dao.Comment
+		DB.Table("comments").Where("video_id = ?", id).Find(&comments)
+		if len(comments) == 0 {
+			continue
+		}
+		key := fmt.Sprintf("cids_%s", id)
+		pairs := make([]redis.Pair, len(comments))
+		for i, c := range comments {
+			pairs[i].Score = c.CreateTime
+			pairs[i].Key = strconv.FormatInt(c.Id, 10)
+		}
+		_, _ = Redis.Zadds(key, pairs...)
+	}
 }
